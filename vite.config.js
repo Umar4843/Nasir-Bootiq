e9f8bfe6d7a274c789dbd7334e81abf7b8bd83fc
Saith Umar <saithumar3999@gmail.com>
2025-04-04 12:20:43 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 600, // Increases chunk warning limit (temporary fix)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor'; // Separate React-related dependencies
-            }
-            return 'vendor'; // Separate other node_modules dependencies
-          }
-        },
-      },
-    },
-  },
 });
